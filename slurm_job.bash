647d887aab1ce39dc250728d82a7b689031108f7
techn0z <minhtrietamoled@gmail.com>
2025-07-09 12:27:56 +0700

    update

---
+++ slurm_job.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCh --account=nhdang
+#SBATCH --job-name=triet_ptnk
+#SBATCH --output=slurm_output/output_%j.log
+#SBATCH --error=slurm_output/error_%j.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=24G
+#SBATCH --gres=gpu:1 
+
+
+conda activate triet_ptnk
+python train.py epochs 50
