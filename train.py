2148c6bb3262bad2fa6e48e0a0ba4ce9facc354a
techn0z <minhtrietamoled@gmail.com>
2025-07-10 12:35:11 +0700

    update

--- train.py
+++ train.py
@@ -19,8 +19,8 @@ def get_arg(name, default = None):
 
 answer_max_length = 40
 
-epochs = int(get_arg("epochs", 100))
-batch_size = int(get_arg("batch_size", 64))
+epochs = int(get_arg("epochs", 50))
+batch_size = int(get_arg("batch_size", 32))
 
 train_dl, test_dl, val_dl, tokenizer = load_saved_data(batch_size = batch_size)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
