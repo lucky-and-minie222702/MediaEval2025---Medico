9a0b0562be6c229d85c8abde81222c742867936f
techn0z <minhtrietamoled@gmail.com>
2025-08-07 09:47:20 +0700

    update

--- train.py
+++ train.py
@@ -83,9 +83,9 @@ training_args = Seq2SeqTrainingArguments(
     bf16 = True,
     report_to = "none",
     
-    dataloader_num_workers = 4,
+    dataloader_num_workers = 0,
     dataloader_pin_memory = False,
-    dataloader_persistent_workers = True,
+    dataloader_persistent_workers = False,
 
     disable_tqdm = not config["tqdm"],
 )
