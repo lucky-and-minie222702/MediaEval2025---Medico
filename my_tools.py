868584e0b1f0937b93a8dc3f0df2b31e8a66b32d
techn0z <minhtrietamoled@gmail.com>
2025-08-08 22:06:47 +0700

    update

--- my_tools.py
+++ my_tools.py
@@ -98,7 +98,7 @@ class MyUtils:
             }
             
             for i in range(n_returns):
-                scores = MyUtils.get_scores_from_ids(self.processor, pred[::, i], label)
+                scores = MyText.get_scores(self.processor, pred[::, i], label)
                 for k, v in scores.items():
                     all_scores[k].append(v)
             
