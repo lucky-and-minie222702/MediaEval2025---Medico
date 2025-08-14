2cd6e040409be57b8230d1e5c1c9b21f12c58c79
techn0z <minhtrietamoled@gmail.com>
2025-08-14 22:05:11 +0700

    update

--- my_tools.py
+++ my_tools.py
@@ -37,8 +37,6 @@ class TrainerSaveLossCallback(TrainerCallback):
 
 QUANT_CONFIG = BitsAndBytesConfig(
     load_in_4bit = True,
-    bnb_4bit_use_double_quant = True,
-    bnb_4bit_compute_dtype = torch.bfloat16,
 )
 
 
