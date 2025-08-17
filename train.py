eb102c5bc8494b80f40b0c3f171b0a39ac397d27
techn0z <minhtrietamoled@gmail.com>
2025-08-17 10:57:31 +0700

    update

--- train.py
+++ train.py
@@ -67,6 +67,7 @@ training_args = Seq2SeqTrainingArguments(
     per_device_eval_batch_size = config.get("val_batch_size", config["batch_size"]),
 
     gradient_accumulation_steps = config["grad_accum"],
+    eval_accumulation_steps = config.get("val_accum", None),
     
     eval_strategy = "steps",
     eval_steps = config["val_steps"],
