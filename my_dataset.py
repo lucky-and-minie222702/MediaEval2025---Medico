d99054f90a5214b946e0fb01708bb03756266a56
techn0z <minhtrietamoled@gmail.com>
2025-08-28 20:33:35 +0700

    update

--- my_dataset.py
+++ my_dataset.py
@@ -140,7 +140,7 @@ class MyDataset(Dataset):
             ans = " ".join(ans)
             return ans
         
-        df["caption"] = [to_caption(d) for d in tqdm(df.iloc, desc = "Generating caption")]
+        df["caption"] = [to_caption(d) for d in tqdm(df.iloc, desc = "Generating caption", total = len(df))]
         self.raw_data = df
         self.data = df.to_dict(orient = 'records')
         self.question_dict = question_dict
