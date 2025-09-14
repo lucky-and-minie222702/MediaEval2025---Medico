8632654d31c60f37a088f8f99f2c801d3b21bff8
techn0z <minhtrietamoled@gmail.com>
2025-09-14 14:27:40 +0700

    update

--- download_data.py
+++ download_data.py
@@ -10,7 +10,6 @@ ds = load_dataset("SimulaMet/Kvasir-VQA-x1")
 
 df = ds['train'].to_pandas()
 df.to_csv(f"data/train.csv", index=False)
-exit()
 
 df = ds['test'].to_pandas()
 df.to_csv(f"data/test.csv", index=False)
