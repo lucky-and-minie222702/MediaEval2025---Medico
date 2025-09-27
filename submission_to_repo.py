0be084df4706e6af9330072894319cfe1a7b7f3e
techn0z <minhtrietamoled@gmail.com>
2025-09-27 14:49:32 +0700

    update2

---
+++ submission_to_repo.py
@@ -0,0 +1,14 @@
+from huggingface_hub import HfApi
+
+api = HfApi()
+api.create_repo(
+    repo_id = "trietbui/mediaeval-medico-submission",
+    private = False,
+    exist_ok = True,
+)
+
+api.upload_file(
+    path_or_fileobj = "submission_task1.py",
+    path_in_repo = "submission_task1.py",
+    repo_id = "trietbui/mediaeval-medico-submission",
+)
