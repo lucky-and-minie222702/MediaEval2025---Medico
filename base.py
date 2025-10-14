2596ca6378c047527d624112851dd29388e5ee03
techn0z <minhtrietamoled@gmail.com>
2025-10-14 15:33:57 +0700

    update2

--- base.py
+++ base.py
@@ -267,8 +267,9 @@ class CausalDataset(BaseDataset):
         inp = self.processor(
             text = inp_text,
             images = self.img,
-            padding = False,
-            truncation = False,
+            padding = "max_length",
+            truncation = True,
+            max_length = self.max_length,
             return_tensors = "pt"
         )
         inp = {k: v.squeeze(0) for k, v in inp.items()}
@@ -279,8 +280,9 @@ class CausalDataset(BaseDataset):
         merge = self.processor(
             text = merge_text,
             images = self.img,
-            padding = False,
-            truncation = False,
+            padding = "max_length",
+            truncation = True,
+            max_length = self.max_length,
             return_tensors = "pt"
         )
         merge = {k: v.squeeze(0) for k, v in merge.items()}
@@ -294,18 +296,10 @@ class CausalDataset(BaseDataset):
             label[:inp_len:] = -100
 
             merge["labels"] = label
-
-            merge["input_ids"] = ModelUtils.pad_and_trunc(merge["input_ids"], self.max_length, self.processor.tokenizer.pad_token_id)
-            merge["attention_mask"] = ModelUtils.pad_and_trunc(merge["attention_mask"], self.max_length, 0)
-            merge["labels"] = ModelUtils.pad_and_trunc(merge["labels"], self.max_length, -100)
         elif self.mode == "infer":
             label = merge["input_ids"].clone()[inp_len::]
             merge = inp
             merge["labels"] = label
-
-            merge["input_ids"] = ModelUtils.pad_and_trunc(merge["input_ids"], self.max_length, self.processor.tokenizer.pad_token_id, side = "left")
-            merge["attention_mask"] = ModelUtils.pad_and_trunc(merge["attention_mask"], self.max_length, 0, side = "left")
-            merge["labels"] = ModelUtils.pad_and_trunc(merge["labels"], self.max_length, -100, side = "left")
             
         return merge
     
