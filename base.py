b9390ed1dfcb01b6dae06f6ebbab665084ae79ae
techn0z <minhtrietamoled@gmail.com>
2025-10-14 17:53:30 +0700

    update2

--- base.py
+++ base.py
@@ -302,7 +302,7 @@ class CausalDataset(BaseDataset):
         assistant_token_id = self.processor.tokenizer.convert_tokens_to_ids(ASSISTANT_TEXT[self.setting])
         
         print(merge["input_ids"])
-        print(self.processor.tokenizer.decode(merge["input_ids"], skip_special_tokens = True))
+        print(self.processor.tokenizer.decode(merge["input_ids"], skip_special_tokens = False))
         print(assistant_token_id)
         
         assistant_idx = find_first_token_position(merge["input_ids"], assistant_token_id)
